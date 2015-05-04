3cb8c9d15b40b45df24301a8c1c18788aabd09da
Jonathan Mann <mann17@gmail.com>
2015-05-03 22:27:47 -0400

    a_star backup

--- path/a_star.py
+++ path/a_star.py
@@ -5,6 +5,8 @@ class Graph(object):
 		self.h = h
 		self.a = a
 		self.p = []
+		self.visited = set() 
+		self.frontier = {}
 	def __str__(self):
 		return ' -> '.join(self.p)
 	def get_cost(self,orig,dest):
@@ -36,8 +38,8 @@ class Graph(object):
 			i += 1
 		return self
 
-h = {'Arad':366,'Timisoara':329,'Zerind':374,'Oradea':380,'Sibiu':253,'Fagaras':176,'Rimnicu':193,'Pitesti':100,'Craiova':160,'Bucharest':0}
-a = {'Arad' : {'Timisoara':118,'Zerind':75,'Sibiu':140},'Timisoara':{'Arad':118,'Craiova':376},'Craiova':{'Rimnicu':146,'Timisoara':376,'Pitesti':138},'Zerind':{'Arad':75,'Oradea':71},'Oradea':{'Sibiu':151,'Zerind':71},'Sibiu':{'Arad': 140,'Oradea':151,'Fagaras':99,'Rimnicu':80},'Fagaras':{'Sibiu':99,'Bucharest':211},'Rimnicu':{'Sibiu':80,'Craiova':146,'Pitesti':97},'Pitesti':{'Rimnicu':97,'Craiova':138,'Bucharest':101},'Bucharest':{'Fagaras':211,'Pitesti':101}}
+h = {'Arad':366,'Timisoara':329,'Zerind':374,'Oradea':380,'Sibiu':253,'Fagaras':176,'Rimnicu':193,'Pitesti':100,'Craiova':160,'Bucharest':0,'Nowhere':266}
+a = {'Arad' : {'Nowhere':100,'Timisoara':118,'Zerind':75,'Sibiu':140},'Timisoara':{'Arad':118,'Craiova':376},'Craiova':{'Rimnicu':146,'Timisoara':376,'Pitesti':138},'Zerind':{'Arad':75,'Oradea':71},'Oradea':{'Sibiu':151,'Zerind':71},'Sibiu':{'Arad': 140,'Oradea':151,'Fagaras':99,'Rimnicu':80},'Fagaras':{'Sibiu':99,'Bucharest':211},'Rimnicu':{'Sibiu':80,'Craiova':146,'Pitesti':97},'Pitesti':{'Rimnicu':97,'Craiova':138,'Bucharest':101},'Bucharest':{'Fagaras':211,'Pitesti':101},'Nowhere':{'Arad':100}}
 
 Hungary = Graph(h,a)
 #grid = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
