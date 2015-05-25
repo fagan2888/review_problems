1a78e64abc554e035748561e25f3d3fef14f0051
Jonathan Mann <mann17@gmail.com>
2015-05-25 11:42:57 -0400

    remove leetcode

--- leetcode/regular_expression_matching.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-	def isMatch(self, s, p):
-		if s == p: return True
-		s = s.split()
-		p = p.split()
-		if p[0] != '*' and p[0] != '.' and p[0] != s[0]: return False	
-
-s = Solution()
-s.isMatch("aa","a")
